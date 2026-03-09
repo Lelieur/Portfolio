c597b2197f0536a65eded72c6cb277c1c04e9cc0
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2026-03-09 21:09:22 +0100

    fix: refactor layout to remove unused font imports and adjust class names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Lucas Lelieur · Web Developer",
@@ -75,7 +59,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased max-w-screen-sm mx-auto w-full px-4 py-12 md:px-0 md:py-16 space-y-24`}
+        className="antialiased max-w-screen-sm mx-auto w-full px-4 py-12 md:px-0 md:py-16 space-y-24"
       >
         <Header />
         <NuqsAdapter>{children}</NuqsAdapter>
