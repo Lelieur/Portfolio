007f2ad9339f80687c9f3c768e4c6ac716012bb9
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2026-03-09 21:12:33 +0100

    chore: remove unused thoughts data file

--- src/data/thoughts.ts
+++
@@ -1,9 +0,0 @@
-export const thoughts = [
-  {
-    title: 'The Future of Web Development',
-    description: '',
-    link: '/thoughts/the-future-of-web-development',
-    year: '2025',
-    slug: 'future-web-development',
-  },
-];
